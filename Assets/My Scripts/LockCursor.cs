3b3650fcc4abc046bb194d0fc61190ecff2aa49e
Feek <fpm5022@gmail.com>
2016-02-18 13:53:42 -0500

    hide cursor script

---
+++ Assets/My Scripts/LockCursor.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockCursor : MonoBehaviour
+{
+	bool isCursorLocked;
+
+	// Use this for initialization
+	void Start ()
+	{
+		isCursorLocked = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		CheckForInput ();
+		CheckIfCursorShouldBeLocked ();
+	}
+
+	void ToggleCursorState ()
+	{
+		isCursorLocked = !isCursorLocked;
+	}
+
+	void CheckForInput ()
+	{
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			ToggleCursorState ();
+		}
+	}
+
+	void CheckIfCursorShouldBeLocked ()
+	{
+		if (isCursorLocked) {
+			Cursor.lockState = CursorLockMode.Locked; // locks cursor to middle of screen
+			Cursor.visible = false; // hides cursor
+		} else {
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+		}
+	}
+}
