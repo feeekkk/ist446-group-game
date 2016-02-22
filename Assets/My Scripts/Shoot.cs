6c8e2bfa19f449af2b8cd9230d855694441fdc75
Feek <fpm5022@gmail.com>
2016-02-22 01:22:09 -0500

    muzzle flash animation

--- Assets/My Scripts/Shoot.cs
+++ Assets/My Scripts/Shoot.cs
@@ -7,11 +7,13 @@ public class Shoot : MonoBehaviour
 	float nextFire = 0f;
 	RaycastHit hit;
 	float range = 300f;
+	GameObject flash;
 
 	// Use this for initialization
 	void Start ()
 	{
-	
+		//flash = (GameObject)GetComponentInChildren<Plane> ();
+		flash = GameObject.Find ("Muzzle Flash");
 	}
 	
 	// Update is called once per frame
@@ -23,15 +25,21 @@ public class Shoot : MonoBehaviour
 	void CheckForInput ()
 	{
 		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
-			Debug.DrawRay (transform.position, transform.forward, Color.green, 3f);
-			if (Physics.Raycast (transform.position, transform.forward, out hit, range)) {
-				if (hit.transform.CompareTag ("Enemy")) {
-					Debug.Log ("Enemy " + hit.transform.name);
-				} else {
-					Debug.Log ("did not hit an enemy");
-				}
-			}
+			FireWeapon ();
 			nextFire = Time.time + fireRate;
+		}
+	}
+
+	void FireWeapon ()
+	{
+		Debug.DrawRay (transform.position, transform.forward, Color.green, 3f);
+		if (Physics.Raycast (transform.position, transform.forward, out hit, range)) {
+			flash.GetComponent<Animation> ().Play ();
+			if (hit.transform.CompareTag ("Enemy")) {
+				Debug.Log ("Enemy " + hit.transform.name);
+			} else {
+				Debug.Log ("did not hit an enemy");
+			}
 		}
 	}
 }
