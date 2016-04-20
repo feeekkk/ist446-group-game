a4fb3f7fbc4ea79c350183c04cfa47f00e1ed431
jcrafts318 <jcrafts318@gmail.com>
2016-04-20 16:47:56 -0400

    Added Scenes For Network Menu and Player Prefab Models

---
+++ Assets/My Scripts/HostMenu.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class HostMenu : MonoBehaviour {
+
+	public InputField ip;
+	public InputField score;
+	public GameObject startButton;
+	public GameObject startText;
+	public GameObject mainButton;
+	public GameObject mainText;
+	private bool startHover = false;
+	private bool mainHover = false;
+
+	// Use this for initialization
+	void Start () {
+		startButton.AddComponent<Rigidbody> ();
+		mainButton.AddComponent<Rigidbody> ();
+		startButton.GetComponent<Rigidbody> ().constraints = RigidbodyConstraints.FreezeAll;
+		mainButton.GetComponent<Rigidbody> ().constraints = RigidbodyConstraints.FreezeAll;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit, 20)) {
+			if (hit.collider != null) {
+				if (hit.collider.attachedRigidbody.gameObject == startButton) {
+					startHover = true;
+					mainHover = false;
+				}
+				else if (hit.collider.attachedRigidbody.gameObject == mainButton) {
+					startHover = false;
+					mainHover = true;
+				}
+			}
+		}
+		else
+		{
+			startHover = false;
+			mainHover = false;
+		}
+		if (startHover && Input.GetMouseButtonDown(0)) {
+			print ("starting game on " + ip.text + " with score limit " + score.text);
+		} else {
+
+		}
+		if (mainHover && Input.GetMouseButtonDown(0)) {
+			SceneManager.LoadScene("NetworkSelect");
+		} else {
+
+		}
+	}
+}
---
+++ Assets/My Scripts/NetworkSelect.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class NetworkSelect : MonoBehaviour {
+
+	public GameObject hostButton;
+	public GameObject hostText;
+	public GameObject joinButton;
+	public GameObject joinText;
+	private bool hostHover = false;
+	private bool joinHover = false;
+
+	// Use this for initialization
+	void Start () {
+		hostButton.AddComponent<Rigidbody> ();
+		joinButton.AddComponent<Rigidbody> ();
+		hostButton.GetComponent<Rigidbody> ().constraints = RigidbodyConstraints.FreezeAll;
+		joinButton.GetComponent<Rigidbody> ().constraints = RigidbodyConstraints.FreezeAll;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit, 20)) {
+			if (hit.collider != null) {
+				if (hit.collider.attachedRigidbody.gameObject == hostButton) {
+					hostHover = true;
+					joinHover = false;
+				}
+				else if (hit.collider.attachedRigidbody.gameObject == joinButton) {
+					hostHover = false;
+					joinHover = true;
+				}
+			}
+		}
+		else
+		{
+			hostHover = false;
+			joinHover = false;
+		}
+		if (hostHover && Input.GetMouseButtonDown(0)) {
+			SceneManager.LoadScene("HostMenu");
+		} else {
+
+		}
+		if (joinHover && Input.GetMouseButtonDown(0)) {
+			SceneManager.LoadScene("JoinMenu");
+		} else {
+
+		}
+	}
+}
