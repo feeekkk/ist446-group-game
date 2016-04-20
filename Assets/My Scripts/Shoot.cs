8941837748c017a7a723fb1a5c4b9fbb68b348e0
Feek <fpm5022@gmail.com>
2016-04-20 16:29:29 -0400

    some restructuring and added weapon data.. may have broken something here down the line we'll see

--- Assets/My Scripts/Shoot.cs
+++ Assets/My Scripts/Shoot.cs
@@ -3,18 +3,16 @@ using System.Collections;
 
 public class Shoot : MonoBehaviour
 {
-	// TO DO: All of this stuff should go into a weapon struct or something
-	float fireRate = 0.3f;
 	float nextFire = 0f;
 	RaycastHit hit;
-	float range = 300f;
 	Transform flash;
-	int damage = 25;
+	WeaponData wd;
 
 	// Use this for initialization
 	void Start ()
 	{
 		flash = transform.Find ("M4A1").Find ("Muzzle Flash");
+		wd = gameObject.GetComponent<WeaponData> ();
 	}
 
 	/**
@@ -24,20 +22,20 @@ public class Shoot : MonoBehaviour
 	{
 		if (Time.time > nextFire) {
 			Debug.DrawRay (transform.position, transform.forward, Color.green, 3f);
+			flash.GetComponent<Animation> ().Play ();
 
-			if (Physics.Raycast (transform.position, transform.forward, out hit, range)) {
-				flash.GetComponent<Animation> ().Play ();
+			if (Physics.Raycast (transform.position, transform.forward, out hit, wd.range)) {
 
 				if (hit.transform.GetComponent<Health> ()) {
 					// this collider has health, lets damage it
-					//Debug.Log ("bullet hit " + hit.transform.name);
-					hit.transform.GetComponent<Health> ().TakeDamage (damage);
+					Debug.Log ("bullet hit " + hit.transform.name);
+					hit.transform.GetComponent<Health> ().TakeDamage (wd.damage);
 				} else {
-					//Debug.Log ("shot missed");
+					Debug.Log ("shot missed");
 				}
 			}
 
-			nextFire = Time.time + fireRate; // reset fire rate
+			nextFire = Time.time + wd.fireRate; // reset fire rate
 			return true;
 		}
 
